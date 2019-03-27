098f99c02ae132cb6d4e8782031fe641ed1e2301
Daniel Juventude <deljuven@ymail.com>
2019-03-27 10:21:22 -0300

    first commit

---
+++ trabalho2/LMSTrainer.py
@@ -0,0 +1,14 @@
+
+class LMSTrainer(object):
+	"""docstring for LMSTrainer"""
+	def __init__(self, arg):
+		super(LMSTrainer, self).__init__()
+		self.arg = arg
+
+	def fit():
+		pass
+
+	def predict():
+		pass
+		
+		
