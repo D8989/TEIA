e9e5d8edcdc9ae4dd0786e1d4249c44dc2ff9930
Daniel Juventude <deljuven@ymail.com>
2019-04-16 21:47:02 -0300

    correcoes da regrecao logistica - nao terminado

--- programas/DrawingGraph.py
+++ programas/DrawingGraph.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
+
 class DrawingGraph:
-    
+
     @staticmethod
-    def drawPoint(x, y):
+    def draw_point(x, y):
         plt.scatter(x, y, s=10, c='r' )
-    
+
     @staticmethod
     def draw(x, f, title): # valores x e função f
         plt.plot(x, [f(a) for a in x])
@@ -13,6 +14,23 @@ class DrawingGraph:
         plt.show()
     
     @staticmethod
-    def drawPoint2(x, y):
-        plt.scatter(x, y, s=10, c='b' )
+    def draw_point2(x, y):
+        plt.scatter(x, y, s=10, c='b')
         plt.show()
+
+    @staticmethod
+    def draw_classification(x, y):
+        temp0y = []
+        temp0x = []
+        temp1y = []
+        temp1x = []
+        for i, yi in enumerate(y):
+            if yi == 0:
+                temp0y += [yi]
+                temp0x += [x[i]]
+            else:
+                temp1y += [yi]
+                temp1x += [x[i]]
+        plt.scatter(temp0x, temp0y, s=10, c='r')
+        plt.scatter(temp1x, temp1y, s=10, c='b')
+
--- programas/main.py
+++ programas/main.py
@@ -1,6 +1,6 @@
 from Regrecao_logistica import LMSTrainer
+from DrawingGraph import DrawingGraph
 import numpy as np
-import DrawingGraph as dg
 
 
 x_train = np.array([[1,2],[1,3],[1,4]])
@@ -9,14 +9,14 @@ y_train = np.array([0,0,1])
 teste = np.array([x[1] for x in x_train])
 trainer = LMSTrainer([1,1], 0.1, 1)
 
-#dg.drawPoint(teste, y_train)
-#dg.draw(teste, trainer.funcAfim, "hipotese")
+#DrawingGraph.draw_point(teste, y_train)
+#DrawingGraph.draw(teste, trainer.funcAfim, 'hipotese')
 print("deltas: ", trainer.getDelta())
 
 pred = trainer.fit(x_train, y_train)
 
-#dg.drawPoint(teste, y_train)
-#dg.draw(teste, trainer.funcAfim, "resltado")
+#DrawingGraph.draw_point(teste, y_train)
+#DrawingGraph.draw(teste, trainer.funcAfim, "resltado")
 print("deltas: ", trainer.getDelta())
 
 print(pred.predict(5))
